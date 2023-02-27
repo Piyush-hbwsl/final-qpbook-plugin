2b28fcf65541c9ef733feb05999197e7ebc3ae52
Piyush Agarwal <piyush.agarwal@hbwsl.com>
2023-02-27 16:28:46 +0530

    created non-hierarchical category book categories

--- admin/class-wp-book-admin.php
+++ admin/class-wp-book-admin.php
@@ -137,4 +137,44 @@ class Wp_Book_Admin {
 
 		register_post_type( 'Book', $args );
 	}
+
+
+	// create custom hierarchical category book
+	public function custom_category_book() {
+
+		$labels = array(
+			'name'                       => _x( 'Book Categories', 'Taxonomy General Name', 'text_domain' ),
+			'singular_name'              => _x( 'Book Category', 'Taxonomy Singular Name', 'text_domain' ),
+			'menu_name'                  => __( 'Book Category', 'text_domain' ),
+			'all_items'                  => __( 'All Items', 'text_domain' ),
+			'parent_item'                => __( 'Parent Item', 'text_domain' ),
+			'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+			'new_item_name'              => __( 'Add Book Category', 'text_domain' ),
+			'add_new_item'               => __( 'Add New Book Category', 'text_domain' ),
+			'edit_item'                  => __( 'Edit Book Category', 'text_domain' ),
+			'update_item'                => __( 'Update Book Category', 'text_domain' ),
+			'view_item'                  => __( 'View Book Category', 'text_domain' ),
+			'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+			'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+			'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+			'popular_items'              => __( 'Popular Items', 'text_domain' ),
+			'search_items'               => __( 'Search Items', 'text_domain' ),
+			'not_found'                  => __( 'Not Found', 'text_domain' ),
+			'no_terms'                   => __( 'No items', 'text_domain' ),
+			'items_list'                 => __( 'Items list', 'text_domain' ),
+			'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+		);
+
+		$args = array(
+			'labels'            => $labels,
+			'hierarchical'      => true,
+			'public'            => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'show_in_nav_menus' => true,
+			'show_tagcloud'     => true,
+		);
+
+		register_taxonomy( 'Book Category', array('book'), $args );
+	}
 }
